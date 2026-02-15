e79cb51ab2afcc49169251350dc0eaf4d1f4bba9
Chandan Kumar <chandan36024@gmail.com>
2026-02-15 19:14:55 +0530

    contact connected to backend

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,16 +7,33 @@ export default function Contact() {
     email: "",
     message: "",
   });
+  const [status,setStatus]=useState('')
 
   const handleChange = (e) =>
     setForm({ ...form, [e.target.name]: e.target.value });
 
-  const handleSubmit = (e) => {
+  const handleSubmit =async (e) => {
     e.preventDefault();
-    alert("Message sent!");
-    setForm({ name: "", email: "", message: "" });
-  };
+    
+    const res=await fetch('http://localhost:3000/api/contact',{
+      method:"POST",
+      headers:{
+        "content-Type" :"application/json",
+      },
+      body:JSON.stringify(form)
+    })
+    
+    const result = await res.json()
+    console.log('backend response :',result)
+     setStatus(result.message)
+     setForm({name:'',email:'',message:''})
+     setTimeout(()=>{
+      setStatus('')
+     },2000)
 
+
+  };
+ 
   return (
     <section className="py-20 bg-slate-950">
       <div className="max-w-6xl mx-auto px-4">
@@ -28,16 +45,22 @@ export default function Contact() {
           <form
             onSubmit={handleSubmit}
             className="bg-slate-900 p-6 rounded-xl space-y-4"
-          >
+          > {status && (
+            <div className="bg-green-600 text-white p-3 rounded text-center">
+              {status}
+            </div>
+          )}
             <input
               name="name"
               value={form.name}
               onChange={handleChange}
               placeholder="Your Name"
               className="w-full p-2 rounded bg-slate-950"
+              required
             />
             <input
               name="email"
+              type="email" 
               value={form.email}
               onChange={handleChange}
               placeholder="Your Email"
@@ -49,6 +72,7 @@ export default function Contact() {
               onChange={handleChange}
               placeholder="Your Message"
               className="w-full p-2 rounded bg-slate-950"
+              required
             />
             <button className="bg-cyan-500 text-black px-4 py-2 rounded">
               Submit
